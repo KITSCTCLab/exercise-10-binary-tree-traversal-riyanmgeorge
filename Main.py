47b655df8c470a40df5f235f666f1a74c16b7e25
riyanmgeorge <110010573+riyanmgeorge@users.noreply.github.com>
2022-10-13 00:05:40 +0530

    Fourth Commit

--- Main.py
+++ Main.py
@@ -2,22 +2,48 @@ class BinaryTreeNode:
     def __init__(self, data):
         self.data = data
         self.left_child = None
-        self.right_child = None
+        self.right_child = None 
 
-    def insert(root, new_value) -> BinaryTreeNode:
+
+def insert(root, new_value) -> BinaryTreeNode:
     
+    if not root:
+        root = BinaryTreeNode(new_value)
+        return root
+    if new_value < root.data:
+         if root.left_child:
+            insert(root.left_child, new_value)
+         else:
+            root.left_child = BinaryTreeNode(new_value)
+    else:
+        if root.right_child:
+            insert(root.right_child, new_value)
+        else:
+            root.right_child = BinaryTreeNode(new_value)
 
 
 def inorder(root) -> None:
-   
+    if root is None:
+        return
+    inorder(root.left_child)
+    print(root.data, end = " ")
+    inorder(root.right_child) 
 
 
 def preorder(root) -> None:
-    # Write your code here
+    if root is None:
+        return
+    print(root.data, end = " ")
+    preorder(root.left_child)
+    preorder(root.right_child)
 
-
+ 
 def postorder(root) -> None:
-    # Write your code here
+    if root is None:
+        return 
+    postorder(root.left_child)
+    postorder(root.right_child)
+    print(root.data, end = " ")
 
 
 # Do not change the following code
@@ -30,7 +56,7 @@ for item in input_data.split(', '):
         flag = False
     else:
         insert(root, int(item))
-inorder(root)
+inorder(root) 
 print()
 preorder(root)
 print()
